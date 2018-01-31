dcbb9686f32a4e843218394edc5e9d3ded5929f9
GoldenSmearing <derektd2@illinios.edu>
2018-01-31 16:16:16 -0600

    I solved the maze!

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -34,13 +34,30 @@ public class SolveMaze {
          * Feel free to adjust this number if you experiment with other mazes.
          */
         for (int step = 0; step < 1000; step++) {
-            // Implement your maze solving algorithm here
-        }
-
-        if (maze.isFinished()) {
-            System.out.println("You solved the maze!");
-        } else {
-            System.out.println("Try again!");
-        }
+            while (!maze.isFinished()) { // Implement your maze solving algorithm here
+                maze.turnRight();
+                if (maze.canMove()) {
+                    maze.move();
+                } else {
+                    maze.turnLeft();
+                    if (maze.canMove()) {
+                        maze.move();
+                    } else {
+                        maze.turnLeft();
+                        if (maze.canMove()) {
+                            maze.move();
+                        } else {
+                            maze.turnLeft();
+                            maze.move();
+                        }
+                    }
+                }
+            }
+            }
+            if (maze.isFinished()) {
+                System.out.println("You solved the maze!");
+            } else {
+                System.out.println("Try again!");
+            }
     }
 }
